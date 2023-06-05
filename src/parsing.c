ceea8dad2748994077940c501894302b0224875d
Jiun Jye Yim <jyim@u90z02s01.42kl.edu.my>
2023-06-05 18:48:11 +0800

    complete split function

--- src/parsing.c
+++ src/parsing.c
@@ -6,24 +6,31 @@
 /*   By: jyim <jyim@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 13:13:48 by jyim              #+#    #+#             */
-/*   Updated: 2023/05/31 12:01:37 by jyim             ###   ########.fr       */
+/*   Updated: 2023/06/05 18:03:38 by jyim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+int	if_quotes(char input)
+{
+	if (input == SINGLE_QUOTE || input == DOUBLE_QUOTE)
+		return (1);
+	return (0);
+}
 
 int	check_quotes(char *input)
 {
 	char	quote_type;
 	int		boolean;
 	int		i;
-	
+
 	i = -1;
 	quote_type = -1;
 	boolean = 0;
 	while (input[++i])
 	{
-		if (input[i] == SINGLE_QUOTE || input[i] == DOUBLE_QUOTE)
+		if (if_quotes(input[i]))
 		{
 			boolean = 1;
 			quote_type = input[i];
@@ -32,22 +39,19 @@ int	check_quotes(char *input)
 				if (input[i] == quote_type)
 				{
 					boolean = 0;
-					break;
+					break ;
 				}
 			}
 		}
-		if (input[i] == '\0')
-			break ;
 	}
-	// printf("Check Quotes: %d\n", boolean);
 	return (boolean);
 }
 
 char	*insert_line(char *input)
 {
-	char *nextline;
-	char *tmp_input;
-	char *new_input;
+	char	*nextline;
+	char	*tmp_input;
+	char	*new_input;
 
 	tmp_input = input;
 	nextline = readline(">");
@@ -58,38 +62,42 @@ char	*insert_line(char *input)
 	return (new_input);
 }
 
-init_cmdgroups(char *input, t_cmd **cmdgroups)
+void	init_cmdgroups(char *input, t_cmd **cmdgroups)
 {
-	int	i;
-	int pipes;
+	int		i;
+	int		pipes;
 	char	quote_type;
+
 	i = -1;
 	pipes = 0;
-	while(input[++i])
+	while (input[++i])
 	{
-		if(input[i] == SINGLE_QUOTE || input[i] == DOUBLE_QUOTE)
+		if (input[i] == SINGLE_QUOTE || input[i] == DOUBLE_QUOTE)
 		{
 			quote_type = input[i];
 			while (input[++i])
 			{
 				if (input[i] == quote_type)
-					break;
+					break ;
 			}
+			continue ;
 		}
-		if(input[i] == '|')
+		if (input[i] == '|')
 			pipes++;
-		if (input[i] == '\0')
-			break ;
 	}
-	printf("%d\n", pipes);
-	cmdgroups = (t_cmd **)malloc (sizeof(t_cmd *) * pipes + 2);
-	cmdgroups[pipes + 1]->args = NULL;
-	}
+	printf("Number of pipes: %d\n", pipes);
+	*cmdgroups = (t_cmd *)malloc (sizeof(t_cmd) * (pipes + 2));
+	(*cmdgroups)[pipes + 1].args = NULL;
+}
 
-// void parse_cmds(char *input, t_cmd *cmdgroups)
-// {
-// 	init_cmdgroups(input, &cmdgroups);
-	
-// }
+void	parse_cmds(char *input, t_cmd *cmdgroups)
+{
+	char	**splitted;
 
-// int	check_num_closed_quotes(c)
+	printf("Input before split: %s$\n", input);
+	splitted = ft_split_quoted(input, ' ');
+	int	k = -1;
+	while (splitted[++k])
+		printf("splitted %d: %s$\n", k, splitted[k]);
+	// init_cmdgroups(input, &cmdgroups);
+}
