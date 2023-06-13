2e96008ea6e5b447aa91b39e30335d0e9dd4b222
Jiun Jye Yim <jyim@u90z02s01.42kl.edu.my>
2023-06-13 11:58:21 +0800

    Fixed append function for 2D string array

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: jyim <jyim@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 10:45:48 by jyim              #+#    #+#             */
-/*   Updated: 2023/06/10 18:31:55 by jyim             ###   ########.fr       */
+/*   Updated: 2023/06/13 10:48:44 by jyim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	has_pipes(char **splitted)
 	int pipes;
 
 	i = -1;
-	pipes = 0;
+	pipes = 1;
 	while (splitted[++i])
 	{
 		if (!ft_strcmp(splitted[i],"|"))
@@ -47,24 +47,36 @@ int	is_rdr(char *splitted)
 	return (0);
 }
 
-char	**copy_2d(char **src, char *s)
+void free_doublearray(char **array)
+{
+	int	i;
+
+	i = -1;
+	while (array[++i] != NULL)
+		free(array[i]);
+	free(array);
+}
+
+char	**copy_and_free_doublearray(char **src, char *s, int size)
 {
 	int		i;
 	char	**tmp_table;
 
 	i = 0;
-	tmp_table = NULL;
+	tmp_table = (char **)malloc (sizeof(char *) * (size + 2));
 	while (src[i] != NULL)
 	{
 		tmp_table[i] = ft_strdup(src[i]);
-		free(src[i]);
+		// printf("copying %s, i: %d\n", tmp_table[i], i);
 		i++;
 	}
-	free(src);
-	tmp_table[i++] = ft_strdup(s);
-	tmp_table[i] = NULL;
+	free_doublearray(src);
+	// printf("Append string : %s$, i: %d\n", s, i);
+	tmp_table[i] = ft_strdup(s);
+	tmp_table[++i] = NULL;
 	return (tmp_table);
 }
+
 
 char **ft_append_2d(char **args, char *str)
 {
@@ -75,14 +87,15 @@ char **ft_append_2d(char **args, char *str)
 	if (args == NULL)
 	{
 		temp_args = (char **)malloc (sizeof(char *) * 2);
-		temp_args[0] = strdup(str);
+		temp_args[0] = ft_strdup(str);
 		temp_args[1] = NULL;
-		return (args);
+		// printf("temp_args[0]: %s\n", temp_args[0]);
+		return (temp_args);
 	}
 	while (args[i])
 		i++;
-	temp_args = (char **)malloc (sizeof(char *) * i + 2);
-	temp_args = copy_2d(args, str);
-	print_darray(temp_args);
+	// printf("Current size: %d\n", i);
+	temp_args = copy_and_free_doublearray(args, str, i);
+	// print_darray(temp_args);
 	return (temp_args);
 }
